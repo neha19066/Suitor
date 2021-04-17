cb293428c572c2e8e6b50f674d5dfc719dd5ef9a
asejal <55681645+asejal@users.noreply.github.com>
2021-04-17 17:14:48 +0530

    Update queriesIndex.sql

--- queriesIndex.sql
+++ queriesIndex.sql
@@ -4,6 +4,15 @@ use suitor;
 -- PARALEGAL
 -- 'A21s9n5a5A', 'Giselle', 'Astra', 'Hanson', '1989-10-22', 'Male', '19792', '14', '57', '19', '20', 'vestibulum.lorem@ornarelibero.co.uk', '9900412440', 'Paralegal', '38', 'P.O. Box 759, 6311 Arcu Avenue', 'Cirebon', '6444871126', 'West Java', 'LGBTQ Law', '4'
 
+CREATE USER ParalegalU@localhost IDENTIFIED BY 'pass';
+CREATE ROLE Paralegal;    
+GRANT SELECT ON suitor.myDetails TO Paralegal;
+GRANT SELECT ON suitor.allLegalDocs TO Paralegal;
+GRANT SELECT, UPDATE, DELETE, INSERT ON suitor.myEvents TO Paralegal;
+GRANT SELECT ON suitor.allCases TO Paralegal;
+GRANT SELECT ON suitor.myCases TO Paralegal;
+GRANT Paralegal TO ParalegalU@localhost;
+
 
 -- view their personal details
 CREATE OR REPLACE VIEW myDetails AS
@@ -72,6 +81,26 @@ where LLastName = "Griffin";
 -- CUSTOMER/CLIENT
 -- 'I21p5a6t2C', 'MacKensie', 'Trevor', 'Crosby', '1987-04-27', '13540', 'arcu@necmetus.edu', '9729651309', 'Ap #335-2862 Curae; St.', 'Stargard Szczeciński', '80461', 'ZP', '1'
 
+CREATE USER CustomerU@localhost IDENTIFIED BY 'pass';
+CREATE ROLE Customer;    
+GRANT SELECT ON suitor.myDetailsClient TO Customer;
+GRANT SELECT, INSERT, UPDATE, DELETE ON suitor.myEventsClient TO Customer;
+GRANT SELECT ON suitor.allmycasesclient TO Customer;
+GRANT SELECT ON suitor.mybillsClient TO Customer;
+GRANT SELECT ON suitor.bestsuitedlawyer TO Customer;
+GRANT Customer TO CustomerU@localhost;
+
+drop role Customer;
+drop user CustomerU@localhost;
+
+select user, host from MySQL.user;
+
+-- login using \connect --mysql CustomerU@localhost on MySQL Shell
+-- change to sql using \sql
+-- do set role all;
+-- use whatever commands you want now
+
+
 -- View their personal details. 
 create or replace view myDetailsClient as
 select * from individualclients
@@ -103,7 +132,7 @@ where userID = "I21p5a6t2C" and myEventsCLient.when = '2021-05-01 04:30:00';
 
 -- View case details of all their cases
 create or replace view allMyCasesClient as 
-select h.caseID, c.plaintiff, c.lastDateOfActivity, c.flair, c.dateOfFiling, c.duration, c.status, l.userID as LawyerID, l.firstName as LFirstName, l.lastName as LLastName, l.emailID as LEmailID, l.positionAtFirm, l.specialization, l.city as LCity, o.oppositionID, o.firstName as OFirstName, o.lastName as OLastName from lawyer l, handles h, legalcases c, hasa ch, individualclients ic, opposition o, against a
+select distinct h.caseID, c.plaintiff, c.lastDateOfActivity, c.flair, c.dateOfFiling, c.duration, c.status, l.userID as LawyerID, l.firstName as LFirstName, l.lastName as LLastName, l.emailID as LEmailID, l.positionAtFirm, l.specialization, l.city as LCity, o.oppositionID, o.firstName as OFirstName, o.lastName as OLastName from lawyer l, handles h, legalcases c, hasa ch, individualclients ic, opposition o, against a
 where l.userID = h.userID and h.caseID = c.caseID and ch.userID = ic.userID and a.oppositionID = o.oppositionID and a.caseID = c.caseID;
 
 select * from allMyCasesClient; 
@@ -125,6 +154,23 @@ select * from BestSuitedLawyer;
 
 -- LAWYER
 
+CREATE USER LawyerU@localhost IDENTIFIED BY 'pass';
+CREATE ROLE Lawyer;    
+GRANT SELECT, INSERT, UPDATE, DELETE ON suitor.LawyerEvents TO Lawyer;
+GRANT SELECT ON suitor.LawyerDeets TO Lawyer;
+GRANT SELECT, UPDATE ON suitor.LawyerCases TO Lawyer;
+GRANT SELECT ON suitor.otherLawyers TO Lawyer;
+GRANT SELECT ON visibleDocs TO Lawyer;
+GRANT SELECT ON individualsasclients TO Lawyer;
+GRANT SELECT ON companyclients TO Lawyer;
+GRANT Lawyer TO LawyerU@localhost;
+
+-- View their personal details (financial history, track record).
+create or replace view LawyerDeets as 
+select * from Lawyer where userId="A21a0f5x5m";
+
+select * from LawyerDeets;
+
 -- View events (such as current court hearings related to ongoing cases).
 CREATE OR REPLACE VIEW LawyerEvents AS
 select * from calendar
@@ -133,17 +179,17 @@ where userID = "A21a0f5x5m";
 select * from LawyerEvents;
 
 -- add new meeting
-insert into myEventsClient values("A21a0f5x5m", '2021-05-01 02:00:00',"Settlement Meeting");
+insert into LawyerEvents values("A21a0f5x5m", '2021-05-01 02:00:00',"Settlement Meeting");
 
 -- reschedule meeting
-update myEventsClient 
+update LawyerEvents 
 set 
-myEventsClient.when = '2021-05-01 04:30:00'
-where userID = "A21a0f5x5m" and myEventsClient.when = '2021-05-01 02:00:00';
+LawyerEvents.when = '2021-05-01 04:30:00'
+where userID = "A21a0f5x5m" and LawyerEvents.when = '2021-05-01 02:00:00';
 
 -- delete details of cancelled meting
-delete from myEventsClient 
-where userID = "A21a0f5x5m" and myEventsCLient.when = '2021-05-01 04:30:00';
+delete from LawyerEvents
+where userID = "A21a0f5x5m" and LawyerEvents.when = '2021-05-01 04:30:00';
 
 
 -- View case details of all their cases. **** Take userID INPUT. ****
@@ -154,11 +200,10 @@ on legalcases.caseID=handles.caseID and handles.userID="A21a0f5x5m";
 
 select * from LawyerCases;
 
--- View their personal details (financial history, track record).
-create or replace view LawyerDeets as 
-select * from Lawyer where userId="A21a0f5x5m";
-
-select * from LawyerDeets;
+-- update cases won/ increment by 1
+update lawyer
+set casesWon = casesWon+1
+where userID = "A21a0f5x5M";
 
 -- View basic details of peers (not salary).
 create or replace view otherlawyers as
@@ -194,13 +239,18 @@ on hasa.caseID=handles.caseID);
 
 select * from companyclients;
 	 
--- update cases won/ increment by 1
-update lawyer
-set casesWon = casesWon+1
-where userID = "A21a0f5x5M";
-
 -- MANAGING PARTNER
 -- 	O21a0b2d6K	Keely	Duncan	Moody	1986-05-01	Female	18547	20	dictum.magna.Ut@Nulla.net	9490378505	Support Staff	P.O. Box 631, 8421 In Road	Itagüí	76642	Antioquia
+
+CREATE USER MPU@localhost IDENTIFIED BY 'pass';
+CREATE ROLE MP;    
+GRANT SELECT, INSERT, UPDATE, DELETE ON suitor.myeventsmanagement TO MP;
+GRANT SELECT ON suitor.mymanagingpartner TO MP;
+GRANT SELECT, UPDATE, INSERT, DELETE ON suitor.allFinancialTrans TO MP;
+GRANT SELECT ON suitor.ChooseLawyerRatio TO MP;
+GRANT SELECT ON suitor.ChooseLawyerRating TO MP;
+GRANT ALL PRIVILEGES ON suitor.* TO MP;
+GRANT MP TO MPU@localhost;
 
 -- 1. View their personal details. 
 create or replace view myManagingPartner as
@@ -253,6 +303,13 @@ select * From ChooseLawyerRating;
 
 -- OTHER STAFF 
 -- 	O21a0b2d6K	Keely	Duncan	Moody	1986-05-01	Female	18547	20	dictum.magna.Ut@Nulla.net	9490378505	Support Staff	P.O. Box 631, 8421 In Road	Itagüí	76642	Antioquia
+
+CREATE USER StaffU@localhost IDENTIFIED BY 'pass';
+CREATE ROLE Staff;    
+GRANT SELECT, INSERT, UPDATE, DELETE ON suitor.myEventsStaff TO Staff;
+GRANT SELECT ON suitor.myDetailsStaff TO Staff;
+GRANT SELECT, UPDATE, INSERT, DELETE ON suitor.allFinancialTrans TO Staff;
+GRANT Staff TO StaffU@localhost;
 
 -- 1. View their personal details. 
 create or replace view myDetailsStaff as
