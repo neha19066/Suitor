c95575f646ee60624af39d62b354366a4e576016
asejal <55681645+asejal@users.noreply.github.com>
2021-04-18 17:54:49 +0530

    Update queriesIndex.sql

--- queriesIndex.sql
+++ queriesIndex.sql
@@ -43,14 +43,14 @@ where userID = "A21s9n5a5A" and myEvents.when = '2021-05-01 04:30:00';
 
 -- view all case details for all cases in the firm
 create or replace view allCases as 
-select h.caseID, c.plaintiff, c.lastDateOfActivity, c.flair, c.dateOfFiling, c.duration, c.status, ic.userID as ClientID, ic.firstName as CFirstName, ic.lastName as CLastName, ic.emailID as CEmailID, ic.isClient, ic.city as CCity, l.userID as LawyerID, l.firstName as LFirstName, l.lastName as LLastName, l.emailID as LEmailID, l.positionAtFirm, l.specialization, l.city as LCity, o.oppositionID, o.firstName as OFirstName, o.lastName as OLastName from lawyer l, handles h, legalcases c, hasa ch, individualclients ic, opposition o, against a
+select distinct h.caseID, c.plaintiff, c.lastDateOfActivity, c.flair, c.dateOfFiling, c.duration, c.status, ic.userID as ClientID, ic.firstName as CFirstName, ic.lastName as CLastName, ic.emailID as CEmailID, ic.isClient, ic.city as CCity, l.userID as LawyerID, l.firstName as LFirstName, l.lastName as LLastName, l.emailID as LEmailID, l.positionAtFirm, l.specialization, l.city as LCity, o.oppositionID, o.firstName as OFirstName, o.lastName as OLastName from lawyer l, handles h, legalcases c, hasa ch, individualclients ic, opposition o, against a
 where l.userID = h.userID and h.caseID = c.caseID and ch.userID = ic.userID and a.oppositionID = o.oppositionID and a.caseID = c.caseID;
 
 select * from allCases;
 
 -- view/update allowed legal documents for all cases in the firm
 create or replace view allLegalDocs as 
-select d.docID, d.createdOn, d.dateLastModified, d.type, c.caseID, c.lastdateofactivity, c.flair, c.status, c.plaintiff from legaldocuments d, legalcases c
+select distinct d.docID, d.createdOn, d.dateLastModified, d.type, c.caseID, c.lastdateofactivity, c.flair, c.status, c.plaintiff from legaldocuments d, legalcases c
 where d.caseID = c.caseID and d.visibility = 1;
 
 select * from allLegalDocs;
@@ -58,7 +58,7 @@ select * from allLegalDocs;
 
 -- view their own cases
 create or replace view myCases as 
-select caseID, plaintiff, lastDateOfActivity, flair, dateOfFiling, duration, status, ClientID, CFirstName, CLastName, CEmailID, isClient, CCity from allCases 
+select distinct caseID, plaintiff, lastDateOfActivity, flair, dateOfFiling, duration, status, ClientID, CFirstName, CLastName, CEmailID, isClient, CCity from allCases 
 where LawyerID = "A21s9n5a5A";
 
 select * from myCases;
